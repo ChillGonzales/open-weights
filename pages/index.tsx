380f5d5f96fff3a411aa85dc052b6d30bbf16195
ChillGonzales <cmmonroe2010@gmail.com>
2020-12-13 19:34:27 -0500

    buttons and bootstrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,37 +46,13 @@ export default function Home(): JSX.Element {
           Easily create and share workouts.
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="row my-5">
+          <div className="col">
+            <button className="btn btn-primary btn-lg mx-3">Create a Plan</button>
+            <button className="btn btn-secondary btn-lg mx-3">Learn More</button>
+          </div>
         </div>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
